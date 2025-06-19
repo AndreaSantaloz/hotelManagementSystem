08e8f09df3fcc5e5472de13162b20e889011526f
AndreaSantaloz <andreasantaloz@gmail.com>
2025-06-19 13:06:08 +0100

    We connect the Room with Hotel

--- src/main/java/Model/Hotel/Room.java
+++ src/main/java/Model/Hotel/Room.java
@@ -7,10 +7,12 @@ public class Room {
     private final String name;
     private final Double pricePerNight;
 
-    public Room(String id, String name, Double pricePerNight) {
+    private final Hotel hotel;
+    public Room(String id, String name, Double pricePerNight,Hotel hotel) {
         this.id = id;
         this.name = name;
         this.pricePerNight = pricePerNight;
+        this.hotel=hotel;
     }
 
     public String getId() {
@@ -24,17 +26,20 @@ public class Room {
     public Double getPricePerNight() {
         return pricePerNight;
     }
+    public Hotel gethotel() {
+        return hotel;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Room room = (Room) o;
-        return Objects.equals(id, room.id) && Objects.equals(name, room.name) && Objects.equals(pricePerNight, room.pricePerNight);
+        return Objects.equals(id, room.id) && Objects.equals(name, room.name) && Objects.equals(pricePerNight, room.pricePerNight) && Objects.equals(hotel, room.hotel);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, pricePerNight);
+        return Objects.hash(id, name, pricePerNight,hotel);
     }
 }
